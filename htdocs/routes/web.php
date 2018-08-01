66cbb97376e424430b44612aa6e8b7b57701a808
TakuKaneko <takuya.kaneko.4192@gmail.com>
2018-08-01 17:41:04 +0900

    [update] REST API サーバー準備

--- htdocs/routes/web.php
+++ htdocs/routes/web.php
@@ -11,10 +11,11 @@
 |
 */
 
+
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/home', 'TopController');
