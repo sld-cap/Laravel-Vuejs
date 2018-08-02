d273a37ee9e7ff0fc91e663e487989c827006793
Takahiro Fukui <takfukui@pasonatech.co.jp>
2018-08-02 11:02:51 +0900

    [add] コーパス管理画面ようのbladeファイルを作成

---
+++ htdocs/resources/views/corpusadmin.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
+    <script>
+      window.Laravel = {csrfToken: "{{ csrf_token() }}"};
+    </script>
+
+    <title>コーパス管理画面</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  </head>
+  <body>
+    <div id="app">
+      <div class="container">
+        <router-view></router-view>
+      </div>
+    </div>
+  </body>
+  <script src="{{ mix('js/app.js') }}"></script>
+</html>
