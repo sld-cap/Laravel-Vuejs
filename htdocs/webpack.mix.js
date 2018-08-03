6d129687380bbe110f1127404a5957dcc7a29750
Takahiro Fukui <takfukui@pasonatech.co.jp>
2018-08-03 10:58:39 +0900

    [update] js, cssを整理による更新

--- htdocs/webpack.mix.js
+++ htdocs/webpack.mix.js
@@ -16,4 +16,20 @@ mix.js(['resources/assets/js/app.js',
         'resources/assets/js/common/core/apiConfig.js',
         'resources/assets/js/common/ext/functions.js'
       ], 'public/js/app.js')
-  .sass('resources/assets/sass/app.scss', 'public/css');
+  .scripts([
+    'resources/assets/js/common/ext/plugins/jquery.min.js',
+    'resources/assets/js/common/ext/plugins/popper.min.js', 
+    'resources/assets/js/common/ext/plugins/bootstrap.min.js',
+    'resources/assets/js/common/ext/plugins/jquery.circliful.min.js'
+  ], 'public/js/corpusadmin-core.js')
+  .sass('resources/assets/sass/app.scss', 'public/css')
+  .styles([
+    'resources/assets/sass/bootstrap/bootstrap.min.css', 
+    'resources/assets/sass/bootstrap/bootstrap-dashboard.css',
+    'resources/assets/sass/jquery.circliful.css'
+  ], 'public/css/bootstrap.css')
+  .styles([
+    'resources/assets/css/common.css',
+    'resources/assets/css/theme/corpusadmin.css',
+  ], 'public/css/corpusadmin.css')
+  .version();
