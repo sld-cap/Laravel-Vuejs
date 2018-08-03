8a5c4b363194174d189b0328d2c5dc348b1ce9b0
Takahiro Fukui <takfukui@pasonatech.co.jp>
2018-08-03 23:45:12 +0900

    [update] api optionファイル更新

--- htdocs/resources/assets/js/common/core/apiConfig.js
+++ htdocs/resources/assets/js/common/core/apiConfig.js
@@ -1,8 +1,15 @@
 export const API_ENDPOINT_LIST = {
   login: "/api/authenticate",
   logout: "",
-  auth: "/api/me"
+  auth: "/api/me",
 
+  // コーパス管理画面
+  getCorpus: {
+    url: "/stub/data/corpusData.json",
+    method: 'POST',
+    params: {},
+    data: {}
+  }
 };
 
 // const API_OPTION = {
