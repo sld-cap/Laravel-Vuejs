ffef724f44b93bcd43ead59799afb4ed8744cdf3
TakuKaneko <takuya.kaneko.4192@gmail.com>
2018-08-06 16:49:54 +0900

    [update] コーパス情報のサンプルREST API実装

--- htdocs/app/Http/Controllers/CorpusController.php
+++ htdocs/app/Http/Controllers/CorpusController.php
@@ -3,82 +3,89 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Corpus;
 
 class CorpusController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    //
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $res_array = array(
+      'code' => 202,
+      'message' => '',
+      'data' => Corpus::find(1)
+    );
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+    return $res_array;
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
--- htdocs/app/Http/Controllers/UserController.php
+++ htdocs/app/Http/Controllers/UserController.php
@@ -11,66 +11,66 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 
 class UserController extends Controller 
 { 
-    public function index() // ユーザ一覧を返します
-    { 
-        $users = User::all();
-        return $users;
-    } 
+  public function index() // ユーザ一覧を返します
+  { 
+    $users = User::all();
+    return $users;
+  } 
 
-    public function show($id) // ユーザIDをキーにユーザ情報詳細を返します
-    { 
-        $user = User::findOrFail($id); 
-        return $user; 
-    } 
+  public function show($id) // ユーザIDをキーにユーザ情報詳細を返します
+  { 
+    $user = User::findOrFail($id); 
+    return $user; 
+  } 
 
-    public function update(Request $request, $id) // ユーザIDをキーにユーザ情報を更新します
-    { 
-        $user = User::findOrFail($id);
-        $user->update($request->all()); 
-        return $user; 
-    } 
+  public function update(Request $request, $id) // ユーザIDをキーにユーザ情報を更新します
+  { 
+    $user = User::findOrFail($id);
+    $user->update($request->all()); 
+    return $user; 
+  } 
 
-    public function store(Request $request) // ユーザを追加します
-    { 
-        $user = new User; 
-        $user->name = $request['name']; 
-        $user->email = $request['email']; 
-        $user->password = bcrypt($request['password']); 
-        $user->save(); 
-        return $user; 
-    } 
+  public function store(Request $request) // ユーザを追加します
+  { 
+    $user = new User; 
+    $user->name = $request['name']; 
+    $user->email = $request['email']; 
+    $user->password = bcrypt($request['password']); 
+    $user->save(); 
+    return $user; 
+  } 
 
-    public function destroy($id) // ユーザIDをキーにユーザ情報を削除します
-    { 
-        $user = User::findOrFail($id); 
-        $user->delete(); 
-        return ''; 
-    }
+  public function destroy($id) // ユーザIDをキーにユーザ情報を削除します
+  { 
+    $user = User::findOrFail($id); 
+    $user->delete(); 
+    return ''; 
+  }
 
-    public function authenticate(Request $request) // emailとpasswordでアクセストークンを取得して返します
+  public function authenticate(Request $request) // emailとpasswordでアクセストークンを取得して返します
+  {
+    $credentials = $request->only('email', 'password');
+    try 
     {
-        $credentials = $request->only('email', 'password');
-        try 
-        {
-            if (! $token = JWTAuth::attempt($credentials)) 
-            {
-                return response()->json(['error' => 'invalid_credentials'], 401);
-            }
-        } 
-        catch (JWTException $e) 
-        {
-            return response()->json(['error' => 'could_not_create_token'], 500);
-        }
-        $user = User::where('email', $request->email)->first();
-
-        return response()->json(compact('user', 'token'));
-    }
-
-    public function getCurrentUser() // トークンに紐づくユーザ情報を取得して返します
+      if (! $token = JWTAuth::attempt($credentials)) 
+      {
+        return response()->json(['error' => 'invalid_credentials'], 401);
+      }
+    } 
+    catch (JWTException $e) 
     {
-        $user = JWTAuth::parseToken()->authenticate();
-
-        return response()->json(compact('user'));
+      return response()->json(['error' => 'could_not_create_token'], 500);
     }
+    $user = User::where('email', $request->email)->first();
+
+    return response()->json(compact('user', 'token'));
+  }
+
+  public function getCurrentUser() // トークンに紐づくユーザ情報を取得して返します
+  {
+    $user = JWTAuth::parseToken()->authenticate();
+
+    return response()->json(compact('user'));
+  }
 
 } 
