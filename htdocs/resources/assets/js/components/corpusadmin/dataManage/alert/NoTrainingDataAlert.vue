29f10322d31f8f4bbb5f2daf725a426183c1638a
Takahiro Fukui <takfukui@pasonatech.co.jp>
2018-08-10 13:58:34 +0900

    [add] 不足していたアラート系コンポーネント追加

---
+++ htdocs/resources/assets/js/components/corpusadmin/dataManage/alert/NoTrainingDataAlert.vue
@@ -0,0 +1,24 @@
+<template>
+  <Alert v-bind:alert-type="alertClass">
+    <div slot="message">学習データが登録されていません。まずは登録を行いましょう。</div>
+  </Alert>
+</template>
+
+<script>
+import Alert from '../../common/Alert.vue';  
+
+export default {
+  components: {
+    Alert
+  },
+  data() {
+    return {
+      alertClass: 'alert alert-dismissible fade show alert-secondary'
+    };
+  },
+  mounted: function() {
+  },
+  methods: {
+  }
+};
+</script>
