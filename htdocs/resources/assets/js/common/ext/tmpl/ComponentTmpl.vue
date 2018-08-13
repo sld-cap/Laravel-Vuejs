16a383ccac86ec7d6113d0bb728fa3d15d723b53
Takahiro Fukui <takfukui@pasonatech.co.jp>
2018-08-13 14:45:49 +0900

    [add] コンポーネントファイルを作るときの雛形ファイル追加

---
+++ htdocs/resources/assets/js/common/ext/tmpl/ComponentTmpl.vue
@@ -0,0 +1,30 @@
+<template>
+</template>
+
+<style>
+</style>
+
+<script>
+// import *** from '***';
+
+export default {
+  props: [],
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {
+  },
+  created() {
+    Core.log('[created]');
+  },
+  mounted() {
+    Core.log('[mounted]');
+  },
+  updated() {
+    Core.log('[updated]');
+  },
+  methods: {
+  },
+}
+</script>
