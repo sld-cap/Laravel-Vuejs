5179d9395579b07c42be2c24aece28124fbcade0
Takahiro Fukui <takfukui@pasonatech.co.jp>
2018-08-16 10:57:43 +0900

    [add] 教師データ削除モーダル追加

---
+++ htdocs/resources/assets/js/vue/corpusAdmin/dataManage/modal/CompDeleteTrainingDataModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <CommonModal>
+    <div slot="title">テキスト削除</div>
+    <!-- /.title -->
+    <div slot="body">
+      <p>テキストの削除が完了しました。</p>
+    </div>
+    <!-- /.body -->
+    <div slot="footer">
+      <button @click="hideModal" type="button" class="btn btn-primary">閉じる</button>
+    </div>
+    <!-- /.footer -->
+  </CommonModal>
+</template>
+
+<script>
+import * as Core from '../../../../common/core/app';
+import * as Ajax from '../../../../common/core/ajax';
+import ApiConfig from '../../../../common/core/apiConfig';
+import CommonModal from '../../common/modal/Modal';
+
+import { mapGetters } from 'vuex';
+import MultiModalMixin from '../../common/modal/mixins/MultiModalMixin';
+
+export default {
+  name: 'CompDeleteTrainingDataModal',
+  mixins: [MultiModalMixin],
+  components: { CommonModal },
+  props: [],
+  data() {
+    return {
+      currentDataType: this.$store.getters['multiModal/currentDataType'],
+    };
+  },
+  computed: {
+  },
+  mounted() {
+  },
+  methods: {
+  },
+};
+</script>
---
+++ htdocs/resources/assets/js/vue/corpusAdmin/dataManage/modal/DeleteTrainingDataModal.vue
@@ -0,0 +1,63 @@
+<template>
+  <CommonModal>
+    <div slot="title">テキスト削除</div>
+    <!-- /.title -->
+    <div slot="body">
+      <div class="alert alert-danger" role="alert">
+        テキストを削除しようとしています。
+      </div>
+      <!-- /.alert -->
+
+      <div class="default-form-area mt-4">
+        <p>削除をやめる場合は、キャンセルボタンを押してください。</p>
+      </div>
+      <!-- /.default-form-area -->
+    </div>
+    <!-- /.body -->
+    <div slot="footer">
+      <button @click="hideModal" type="button" class="btn btn-secondary">キャンセル</button>
+      <button @click="deleteExec" type="button" class="btn btn-primary">削除する</button>
+    </div>
+    <!-- /.footer -->
+  </CommonModal>
+</template>
+
+<script>
+import * as Core from '../../../../common/core/app';
+import * as Ajax from '../../../../common/core/ajax';
+import ApiConfig from '../../../../common/core/apiConfig';
+import CommonModal from '../../common/modal/Modal';
+
+import { mapGetters } from 'vuex';
+import MultiModalMixin from '../../common/modal/mixins/MultiModalMixin';
+import SetErrDataMixin from '../../common/form/mixins/SetErrData';
+
+export default {
+  name: 'DeleteTrainingDataModal',
+  mixins: [MultiModalMixin, SetErrDataMixin],
+  components: { CommonModal },
+  props: [],
+  data() {
+    return {
+      deleteData: {
+        creative_id: this.$store.getters['multiModal/deleteTrainingData'].creative_id,
+      },
+    };
+  },
+  computed: {
+  },
+  watch: {
+  },
+  created() {
+    Core.log('[crated]');
+  },
+  mounted() {
+    Core.log('[mounted]');
+  },
+  methods: {
+    deleteExec() {
+      this.$store.dispatch('corpusTrainingData/deleteTrainingData', this.deleteData);
+    },
+  },
+};
+</script>
