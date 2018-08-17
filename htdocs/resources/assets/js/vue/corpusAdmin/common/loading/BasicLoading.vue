17a1e60475db917da4f2b236685f1a9302b52661
Takahiro Fukui <takfukui@pasonatech.co.jp>
2018-08-17 10:54:01 +0900

    [add] 各画面でのベーシックなローディングコンポーネントを追加

---
+++ htdocs/resources/assets/js/vue/corpusAdmin/common/loading/BasicLoading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="loading-block">
+    <BallSpinFadeLoader size="5px" color="black"/>
+    <div class="vue-loaders-text pl-1">Loading</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  props: [],
+  data() {
+    return {};
+  },
+  methods: {
+  },
+};
+</script>
+
+<style>
+.vue-loaders,
+.vue-loaders-text {
+  display: inline-block;  /* インラインブロック化 */
+  vertical-align: middle; /* 中央を揃える */
+}
+</style>
